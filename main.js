2a26d30efc2dac8bf06d9644860f1a7bfe930c28
Hestias-space <voxicberry@gmail.com>
2026-03-13 01:05:47 +0200

    setBonus and countBonusPerMonth

--- main.js
+++ main.js
@@ -10,36 +10,36 @@ const fs = require("fs");
 // helper function converts time in hh:mm:ss am or pm format to total seconds
 function toSeconds(time) {
     //      6:12:30    pm
-        let [clock, period] = time.split(" "); // am w pm are seperated by space
+    let [clock, period] = time.split(" "); // am w pm are seperated by space
     //     [ "6", "12", "30"]   
-        let parts = clock.split(":");
+    let parts = clock.split(":");
 
-        //parts is an array of strings 
-        let h = parseInt(parts[0]);
-        let m = parseInt(parts[1]);
-        let s = parseInt(parts[2]);
-        
-        //24 hour conversion!!!
-        if (period === "pm" && h !== 12) h += 12;
-        if (period === "am" && h === 12) h = 0;
+    //parts is an array of strings 
+    let h = parseInt(parts[0]);
+    let m = parseInt(parts[1]);
+    let s = parseInt(parts[2]);
 
-        return h * 3600 + m * 60 + s;
-    }
-    //another helper but for formats like hh:mm:ss without am or pm
+    //24 hour conversion!!!
+    if (period === "pm" && h !== 12) h += 12;
+    if (period === "am" && h === 12) h = 0;
+
+    return h * 3600 + m * 60 + s;
+}
+//another helper but for formats like hh:mm:ss without am or pm
 function toSecondsNoPeriod(time) {
-        let parts = time.split(":");
- 
-        let h = parseInt(parts[0]);
-        let m = parseInt(parts[1]);
-        let s = parseInt(parts[2]);
-        return h * 3600 + m * 60 + s;
-    }
+    let parts = time.split(":");
+
+    let h = parseInt(parts[0]);
+    let m = parseInt(parts[1]);
+    let s = parseInt(parts[2]);
+    return h * 3600 + m * 60 + s;
+}
 //helper function to reconvert seconds to h:mm:ss format + adding zeros to the left 
 function formatTime(totalSeconds) {
     let h = Math.floor(totalSeconds / 3600);
     let m = Math.floor((totalSeconds % 3600) / 60);
     let s = totalSeconds % 60;
-    return h + ":" + String(m).padStart(2,"0") + ":" + String(s).padStart(2,"0");
+    return h + ":" + String(m).padStart(2, "0") + ":" + String(s).padStart(2, "0");
 }
 
 
@@ -67,10 +67,10 @@ function getShiftDuration(startTime, endTime) {
 function getIdleTime(startTime, endTime) {
 
     const deliveryStart = 8 * 3600;   // 8am in seconds
-    const deliveryEnd   = 22 * 3600;  // 10pm in seconds
+    const deliveryEnd = 22 * 3600;  // 10pm in seconds
 
     let shiftStart = toSeconds(startTime);
-    let shiftEnd   = toSeconds(endTime);
+    let shiftEnd = toSeconds(endTime);
 
     // before 8 am
     let before = 0;
@@ -99,10 +99,10 @@ function getIdleTime(startTime, endTime) {
 // ============================================================
 function getActiveTime(shiftDuration, idleTime) {
 
-if(idleTime === "0:00:00") return shiftDuration; // edge case if there is no idle time
-     activeTime = toSecondsNoPeriod(shiftDuration) - toSecondsNoPeriod(idleTime);
+    if (idleTime === "0:00:00") return shiftDuration; // edge case if there is no idle time
+    activeTime = toSecondsNoPeriod(shiftDuration) - toSecondsNoPeriod(idleTime);
 
-     return formatTime(activeTime);
+    return formatTime(activeTime);
 }
 
 // ============================================================
@@ -112,13 +112,13 @@ if(idleTime === "0:00:00") return shiftDuration; // edge case if there is no idl
 // Returns: boolean
 // ============================================================
 function metQuota(date, activeTime) {
-    
+
     const dailyQuota = 8 * 3600 + 24 * 60; // 8 hours and 24 minutes in seconds
     const eidQuota = 6 * 3600; // 6 hours in seconds
 
     let [year, month, day] = date.split("-");
 
-    if(month === "04" && (day >= "10" && day <= "30")) {
+    if (month === "04" && (day >= "10" && day <= "30")) {
         return toSecondsNoPeriod(activeTime) >= eidQuota;
     }
     return toSecondsNoPeriod(activeTime) >= dailyQuota;
@@ -131,8 +131,61 @@ function metQuota(date, activeTime) {
 // Returns: object with 10 properties or empty object {}
 // ============================================================
 function addShiftRecord(textFile, shiftObj) {
-    // TODO: Implement this function
+    //                                     read bytes to normal text
+    let content = fs.readFileSync(textFile, "utf-8");
+    let rows = content.split("\n"); // split text into rows
+
+    //CHECK FOR DUPLICATE with id w dateeee
+    for (let row of rows) {
+        let cols = row.split(",");
+        if (cols[0] === shiftObj.driverID && cols[2] === shiftObj.date) {
+            return {};
+        }
+    }
+    let shiftDuration = getShiftDuration(shiftObj.startTime, shiftObj.endTime);
+    let idleTime = getIdleTime(shiftObj.startTime, shiftObj.endTime);
+    let activeTime = getActiveTime(shiftDuration, idleTime);
+    let quota = metQuota(shiftObj.date, activeTime);
+    // da esmo template literal 3shan ne7ot kol el values fe string wa7da using back tick `` 
+    let newRow = `${shiftObj.driverID},${shiftObj.driverName},${shiftObj.date},${shiftObj.startTime},${shiftObj.endTime},${shiftDuration},${idleTime},${activeTime},${quota},false`;
+
+
+    let lastIndex = -1;
+    for (let i = 0; i < rows.length; i++) {
+        //if same driver ID is found, update lastIndex to current index 
+
+        if (rows[i].split(",")[0] === shiftObj.driverID) {
+            lastIndex = i;
+        }
+    }
+
+    // if lastIndex is -1, it means driverID was not found in the file 
+    if (lastIndex === -1) {
+        rows.push(newRow);
+    } else {
+        rows.splice(lastIndex + 1, 0, newRow); // insert after last record of that same driver 
+    }
+
+    //              rewrites whole text file again but with new row                                     
+    fs.writeFileSync(textFile, rows.join("\n"), "utf8");
+
+    return {
+        driverID: shiftObj.driverID,
+        driverName: shiftObj.driverName,
+        date: shiftObj.date,
+        startTime: shiftObj.startTime,
+        endTime: shiftObj.endTime,
+        shiftDuration,
+        idleTime,
+        activeTime,
+        metQuota: quota,
+        hasBonus: false
+    };
 }
+
+
+
+
 
 // ============================================================
 // Function 6: setBonus(textFile, driverID, date, newValue)
@@ -143,7 +196,18 @@ function addShiftRecord(textFile, shiftObj) {
 // Returns: nothing (void)
 // ============================================================
 function setBonus(textFile, driverID, date, newValue) {
-    // TODO: Implement this function
+    let content = fs.readFileSync(textFile, "utf-8");
+    let rows = content.split("\n");
+
+    for (let i = 0; i < rows.length; i++) {
+        let cols = rows[i].split(",");
+        if (cols[0] === driverID && cols[2] === date) {
+            cols[9] = newValue;
+            rows[i] = cols.join(",");
+        }
+    }
+    fs.writeFileSync(textFile, rows.join("\n"), "utf8");
+
 }
 
 // ============================================================
@@ -154,7 +218,25 @@ function setBonus(textFile, driverID, date, newValue) {
 // Returns: number (-1 if driverID not found)
 // ============================================================
 function countBonusPerMonth(textFile, driverID, month) {
-    // TODO: Implement this function
+    let content = fs.readFileSync(textFile, "utf-8");
+    let rows = content.split("\n");
+
+    let count = 0;
+    let driverFound = false;
+
+    for (let row of rows) {
+        let cols = row.split(",");
+        if (cols[0] === driverID) {
+            driverFound = true;
+            let rowMonth = cols[2].split("-")[1]; //extract month first!!!!
+
+            //                                                  for \n
+            if (parseInt(rowMonth) === parseInt(month) && cols[9].trim() === "true") {
+                count++;
+            }
+        }
+    }
+    return driverFound ? count : -1;
 }
 
 // ============================================================
